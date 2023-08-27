91a5190d845397095014da05cf4687f17a23caac
James Sully <sullyj3@gmail.com>
2023-08-27 13:25:58 +1000

    Part 3

--- player.gd
+++ player.gd
@@ -1,34 +1,38 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -300.0
-const ACCELERATION = 1400
-const FRICTION = 1000
+@export var movement_data : PlayerMovementData
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $"Coyote Jump Timer"
 
 func apply_gravity(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * delta * movement_data.gravity_scale
 
 func handle_jump():
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_accept"):
-			velocity.y = JUMP_VELOCITY
-	else:
-		if Input.is_action_just_released("ui_accept") and velocity.y < 0:
-			velocity.y = velocity.y / 2
+	if Input.is_action_just_pressed("ui_accept"):
+		var can_jump = is_on_floor() or not coyote_jump_timer.is_stopped()
+		if can_jump:
+			velocity.y = movement_data.jump_velocity
+	# slow upwards movement on jump release
+	elif Input.is_action_just_released("ui_accept") and not is_on_floor() and velocity.y < 0:
+		velocity.y = velocity.y / 2
 
 func handle_acceleration(input_axis, delta):
 	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, input_axis * SPEED, ACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, input_axis * movement_data.speed, movement_data.acceleration * delta)
 
+# ground only
 func apply_friction(input_axis, delta):
-	if input_axis == 0:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
+
+func apply_air_resistance(input_axis, delta):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
 
 func _physics_process(delta):
 	apply_gravity(delta)
@@ -37,7 +41,12 @@ func _physics_process(delta):
 	update_animations(input_axis)
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
+	apply_air_resistance(input_axis, delta)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = (was_on_floor and not is_on_floor() and velocity.y >= 0)
+	if just_left_ledge:
+		coyote_jump_timer.start()
 
 func update_animations(input_axis):
 	if input_axis == 0:
--- world.gd
+++ world.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 @onready var collision_polygon_2d = $platform/CollisionPolygon2D
 @onready var polygon_2d = $platform/CollisionPolygon2D/Polygon2D
+@onready var camera = $Camera2D
+@onready var player = $Player
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
 	polygon_2d.polygon = collision_polygon_2d.polygon
 
+func _process(delta):
+	camera.position = player.position
