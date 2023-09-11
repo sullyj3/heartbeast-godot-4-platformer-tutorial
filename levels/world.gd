477017ee433a8e996ddb0bece1298fe9b7d6031e
James Sully <sullyj3@gmail.com>
2023-09-11 21:55:20 +1000

    Part 7: Autotile + Sloped tiles

--- levels/world.gd
+++ levels/world.gd
@@ -1,14 +1,11 @@
 extends Node2D
 
-@onready var collision_polygon_2d = $platform/CollisionPolygon2D
-@onready var polygon_2d = $platform/CollisionPolygon2D/Polygon2D
 @onready var camera = $Camera2D
 @onready var player = $Player
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	polygon_2d.polygon = collision_polygon_2d.polygon
 	Events.level_completed.connect(show_level_completed)
 
 func _process(_delta):
